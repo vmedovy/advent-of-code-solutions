83c2864324de53b0d4c4411aa6f931341a4e8416
Vladimir Medovy <34925522+vmedovy@users.noreply.github.com>
2020-12-02 20:30:48 +0100

    2020-1: Use `sum()` to sum up numbers in collection
    This is a lot simpler than using `reduce()` with a custom lambda for
    adding.

--- 2020/01/python/solve_2020-01.py
+++ 2020/01/python/solve_2020-01.py
@@ -7,6 +7,6 @@ with open('../input') as input_file:
     data = tuple(map(int, input_file))
 
 for num_count in (2, 3):
-    valid_combinations = (c for c in combinations(data, num_count) if reduce(lambda x, y: x + y, c) == TARGET_SUM)
+    valid_combinations = (c for c in combinations(data, num_count) if sum(c) == TARGET_SUM)
     for combination in valid_combinations:
         print(f'{" * ".join(tuple(map(str, combination)))} = {reduce(lambda x, y: x * y, combination)}')
