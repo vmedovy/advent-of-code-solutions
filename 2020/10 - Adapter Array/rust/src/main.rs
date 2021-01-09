888e9db3540289ec665ebe0aa60489525f56fe43
Vladimir Medovy <34925522+vmedovy@users.noreply.github.com>
2021-01-09 22:58:55 +0100

    2020-10: Format Rust code using `rustfmt`

--- 2020/10 - Adapter Array/rust/src/main.rs
+++ 2020/10 - Adapter Array/rust/src/main.rs
@@ -20,7 +20,8 @@ fn calculate_joltage_differences(jolt_ratings_input: &Vec<i32>) -> Vec<i32> {
     full_jolt_ratings.push(full_jolt_ratings.iter().max().unwrap() + 3); // built-in joltage adapter
     full_jolt_ratings.sort();
 
-    full_jolt_ratings.windows(2)
+    full_jolt_ratings
+        .windows(2)
         .map(|slice| slice[1] - slice[0])
         .collect::<Vec<_>>()
 }
@@ -43,7 +44,9 @@ fn solve_part2(jolt_ratings_input: &Vec<i32>) -> usize {
     //-------------------------------------------------------
     // Assuming that the only existing joltage differences are 1 and 3 means that joltage difference
     // sequences split at joltage differences of 3 only consist of 1s.
-    assert!(joltage_differences.iter().all(|elem| *elem == 1 || *elem == 3));
+    assert!(joltage_differences
+        .iter()
+        .all(|elem| *elem == 1 || *elem == 3));
 
     // Assuming a maximum sequence length of 4 and sequences consisting of only 1s allows to count
     // the possible options manually:
@@ -70,13 +73,14 @@ fn solve_part2(jolt_ratings_input: &Vec<i32>) -> usize {
     joltage_difference_sequence_lengths
         .iter()
         .filter(|seq_len| **seq_len > 1)
-        .fold(1, |prod: usize, seq_len: &usize|
+        .fold(1, |prod: usize, seq_len: &usize| {
             prod * match *seq_len {
                 2 => 2,
                 3 => 4,
                 4 => 7,
-                _ => panic!("Unexpected sequence length of {}!", seq_len)
-            })
+                _ => panic!("Unexpected sequence length of {}!", seq_len),
+            }
+        })
 }
 
 fn main() {
@@ -91,11 +95,17 @@ fn main() {
         .collect::<Vec<_>>();
 
     let (one_jolt_diffs, three_jolt_diffs) = solve_part1(&jolt_ratings);
-    println!("Part 1:\n\
-              \tThe number of 1-jolt differences multiplied by the number of 3-jolt\n\
-              \tdifferences is {}.", one_jolt_diffs * three_jolt_diffs);
-    println!("Part 2:\n\tThe number of possible combinations is {}.",
-             solve_part2(&jolt_ratings));
+    println!(
+        "Part 1:\n\
+         \tThe number of 1-jolt differences multiplied by the number of 3-jolt\n\
+         \tdifferences is {}.",
+        one_jolt_diffs * three_jolt_diffs
+    );
+    println!(
+        "Part 2:\n\
+         \tThe number of possible combinations is {}.",
+        solve_part2(&jolt_ratings)
+    );
 }
 
 #[cfg(test)]
@@ -119,37 +129,8 @@ mod part1 {
     #[test]
     fn test_second_example() {
         let input = vec![
-            28,
-            33,
-            18,
-            42,
-            31,
-            14,
-            46,
-            20,
-            48,
-            47,
-            24,
-            23,
-            49,
-            45,
-            19,
-            38,
-            39,
-            11,
-            1,
-            32,
-            25,
-            35,
-            8,
-            17,
-            7,
-            9,
-            4,
-            2,
-            34,
-            10,
-            3
+            28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38, 39, 11, 1, 32, 25, 35,
+            8, 17, 7, 9, 4, 2, 34, 10, 3,
         ];
         assert_eq!(solve_part1(&input), (22, 10));
     }
@@ -168,37 +149,8 @@ mod part2 {
     #[test]
     fn test_second_example() {
         let input = vec![
-            28,
-            33,
-            18,
-            42,
-            31,
-            14,
-            46,
-            20,
-            48,
-            47,
-            24,
-            23,
-            49,
-            45,
-            19,
-            38,
-            39,
-            11,
-            1,
-            32,
-            25,
-            35,
-            8,
-            17,
-            7,
-            9,
-            4,
-            2,
-            34,
-            10,
-            3
+            28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38, 39, 11, 1, 32, 25, 35,
+            8, 17, 7, 9, 4, 2, 34, 10, 3,
         ];
         assert_eq!(solve_part2(&input), 19208);
     }
